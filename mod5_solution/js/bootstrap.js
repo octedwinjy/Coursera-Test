ed1ccf33d904e556d8f607aea95a77a6bc5738d1
octedwinjy <118704570+octedwinjy@users.noreply.github.com>
2022-11-26 08:48:57 +0800

    Update bootstrap.js

--- mod5_solution/js/bootstrap.js
+++ mod5_solution/js/bootstrap.js
@@ -18,7 +18,7 @@ if (typeof jQuery === 'undefined') {
 
 /* ========================================================================
  * Bootstrap: transition.js v3.3.6
- * http://getbootstrap.com/javascript/#transition
+ * http://getbootstrap.com/javascript/#transitions
  * ========================================================================
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
