aa8cc83585f1c7e59a6c260c6fb0d4bbd675f62a
octedwinjy <118704570+octedwinjy@users.noreply.github.com>
2022-11-26 08:44:08 +0800

    Update script.js

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -83,8 +83,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitly setting the flag to get JSON from server processed into an object literal
+  function (reponseText) {
+    document.querySelector("#main-content")
+    .innerHTML = reponseText;
+    // body...
+  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  false); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
